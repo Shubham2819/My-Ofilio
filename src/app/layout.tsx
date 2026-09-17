a960183c8fc148e0e0f7244f3ea892e65e88d025
Shubham Pandey <140235161+Shubham2819@users.noreply.github.com>
2026-09-17 10:02:23 +0530

    layout update eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { siteMetadata }  from "@/components/metadata";
-// @ts-expect-error CSS imports are handled by Next.js
 import "./globals.css";
 
 export const metadata: Metadata = siteMetadata;
