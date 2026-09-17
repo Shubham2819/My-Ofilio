696d68765c9c6cfc2af76bf193177d99c2c5b066
Shubham <shubham@nivotime.com>
2026-09-17 11:00:26 +0530

    fix: remove unused eslint-config-next/typescript import and update next.config.ts to include eslint settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextTs from "eslint-config-next/typescript.js";
 import nextVitals from "eslint-config-next/core-web-vitals.js";
 
 const eslintConfig = defineConfig([
   nextVitals,
-  nextTs,
 
   globalIgnores([
     ".next/**",
